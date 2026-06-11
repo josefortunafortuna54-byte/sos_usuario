63283ecab0a9d9a584eb3b84b86598a49d8fd482
josefortunafortuna54-byte <teu-email-do-github>
2026-06-11 23:53:29 +0100

    forçar compileSdk 36 em todos os plugins

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,6 +5,18 @@ allprojects {
     }
 }
 
+// Forçar compileSdk 36 em todos os subprojectos (plugins incluídos)
+subprojects {
+    afterEvaluate {
+        if (plugins.hasPlugin("com.android.application") ||
+            plugins.hasPlugin("com.android.library")) {
+            extensions.configure<com.android.build.gradle.BaseExtension> {
+                compileSdkVersion(36)
+            }
+        }
+    }
+}
+
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
@@ -15,6 +27,7 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
